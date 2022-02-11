f27748a6e98730b0ae5be9794801f5086e397354
Richy Chen <richy@Richys-MacBook.local>
2022-02-11 08:16:41 -0500

    finished ico

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x5Cb27c76089a38eb9B11fB76AC5A11d0E779f50C";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
